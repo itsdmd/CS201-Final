24a58cf49a05f700bfcc874045342f6894860693
Đức Đào <duc.dao.431@gmail.com>
2023-03-01 16:10:21 +0700

    #4: create simple ui mockup for testing

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,1 +1,24 @@
 console.log("homepage.js loaded");
+
+/* ---------------------------------- */
+/*              Variables             */
+/* ---------------------------------- */
+
+/* -------------- Const ------------- */
+const API_HOST = "reuters-business-and-financial-news.p.rapidapi.com";
+const API_HOST_CTG = "all-category";
+
+/* -------------- Value ------------- */
+let apiKey = "";
+
+/* ------------- Element ------------ */
+let submitBtn = document.getElementById("submit");
+
+/* ---------- EventListener --------- */
+submitBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	console.log("param-submit clicked");
+
+	apiKey = document.querySelector(".param-api").value;
+	console.log("API Key: ", apiKey);
+});
