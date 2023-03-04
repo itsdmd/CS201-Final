62316b9c92acb10fc34c5b88fa7b78d689fa2d3a
Đức Đào <duc.dao.431@gmail.com>
2023-03-04 13:38:11 +0700

    #29: add wikimedia_ctg.js

---
+++ assets/js/data/wikimedia_ctg.js
@@ -0,0 +1,34 @@
+const CATEGORIES = [
+	{
+		id: "all",
+		name: "All",
+		desc: "Returns all types",
+	},
+	{
+		id: "selected",
+		name: "Highlighted",
+		desc: "Curated set of events that occurred on the given date",
+	},
+	{
+		id: "births",
+		name: "Births",
+		desc: "Notable people born on the given date",
+	},
+	{
+		id: "deaths",
+		name: "Deaths",
+		desc: "Notable people who died on the given date",
+	},
+	{
+		id: "holidays",
+		name: "Holidays",
+		desc: "Fixed holidays celebrated on the given date",
+	},
+	{
+		id: "events",
+		name: "Events",
+		desc: "Events that occurred on the given date that are not included in another type",
+	},
+];
+
+export { CATEGORIES };
